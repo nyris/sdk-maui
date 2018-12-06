05c71f5364dfb55defa1e0b2a2ad15c6d64be01e
Sidali Mellouk <s.mellouk@live.com>
2018-12-06 19:20:08 +0100

    [Image Service] [Upg]: Add remote models

--- sdk/nyris.sdk/Network/Model/Links.cs
+++ sdk/nyris.sdk/Network/Model/Links.cs
@@ -1,7 +1,13 @@
+using Newtonsoft.Json;
+
 namespace Nyris.Sdk.Network.Model
 {
     public class Links
     {
+        [JsonProperty(PropertyName = "main")]
+        public string main { get; set; }
         
+        [JsonProperty(PropertyName = "mobile")]
+        public string Mobile { get; set; }
     }
 }
--- sdk/nyris.sdk/Network/Model/OfferResponse.cs
+++ sdk/nyris.sdk/Network/Model/OfferResponse.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace Nyris.Sdk.Network.Model
 {
-    public class OfferResponseBody
+    public class OfferResponse
     {
+        [JsonProperty(PropertyName = "results")] 
+        public List<Offer> Offers { get; set; }
         
+        [JsonProperty(PropertyName = "predicted_category")] 
+        public List<Dictionary<string, float>> PredictedCategories { get; set; }
     }
 }
