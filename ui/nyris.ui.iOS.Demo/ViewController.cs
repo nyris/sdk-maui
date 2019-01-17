b2440f3853803802b59a9d18e1627aa4d9aeb797
Mostefaoui Mohamed Anes <m.anas@outlook.com>
2019-01-17 21:24:40 +0300

    SDK-23_iOS [Demo]: Add usage example in demo

--- ui/nyris.ui.iOS.Demo/ViewController.cs
+++ ui/nyris.ui.iOS.Demo/ViewController.cs
@@ -2,13 +2,15 @@
 
 using UIKit;
 using Foundation;
-using Nyris.UI.iOS.Camera;
+using Nyris.UI.iOS;
+using Nyris.UI.iOS.EventArgs;
 
 
 namespace Nyris.UI.iOS.Demo
 {
     public partial class ViewController : UIViewController
     {
+        private INyrisSearcher searchService;
         protected ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
@@ -17,18 +19,34 @@ namespace Nyris.UI.iOS.Demo
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+            searchService = NyrisSearcher
+                .Builder("API-KEY", this)
+                .CaptureLabelText("My Capture label.")
+                .CameraPermissionDeniedErrorMessage("You can not use this component until you activate the camera permission!")
+                .CameraPermissionRequestIfDeniedMessage("Please authorize camera usage in settings.")
+                .ConfigurationFailedErrorMessage("Camera setup failed")
+                .DialogErrorTitle("Error Title")
+                .AgreeButtonTitle("OK")
+                .CancelButtonTitle("Cancel")
+                .CategoryPrediction();
+            searchService.OfferAvailable += SearchServiceOnOfferAvailable;
+            searchService.RequestFailed += SearchServiceOnRequestFailed;
+        }
+
+        private void SearchServiceOnRequestFailed(object sender, Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SearchServiceOnOfferAvailable(object sender, OfferResponseEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         public override void ViewDidAppear(bool animated)
         {
             base.ViewDidAppear(animated);
-            var bundle = NSBundle.FromClass(new ObjCRuntime.Class(typeof(CameraManager)));
-            var storyboard = UIStoryboard.FromName("CameraController", bundle);
-            var cameraController = storyboard.InstantiateInitialViewController();
-            if (cameraController != null)
-            {
-                this.PresentViewController(cameraController, true, null);
-            }
+            searchService.Start();
         }
 
         public override void DidReceiveMemoryWarning()
