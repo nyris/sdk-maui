e30f3a778734af42fb316e1bd5379df06e5aa718
Mostefaoui Mohamed Anes <m.anas@outlook.com>
2020-04-02 14:54:47 +0300

    [Refactor] Cleanup grid overlay code.

--- ui/nyris.ui.iOS/Crop/CropOverlayView.cs
+++ ui/nyris.ui.iOS/Crop/CropOverlayView.cs
@@ -7,31 +7,31 @@ namespace Nyris.UI.iOS.Crop
 {
     public class CropOverlayView : UIView
     {
-        nfloat _screenWidth = UIScreen.MainScreen.Bounds.Width;
-        nfloat _screenHeight = UIScreen.MainScreen.Bounds.Height;
+        private readonly nfloat _screenWidth = UIScreen.MainScreen.Bounds.Width;
+        private readonly nfloat _screenHeight = UIScreen.MainScreen.Bounds.Height;
 
-        List<UIView> _outerLines;
-        List<UIView> _horizontalLines;
-        List<UIView> _verticalLines;
+        private List<UIView> _outerLines;
+        private List<UIView> _horizontalLines;
+        private List<UIView> _verticalLines;
 
 
-        List<UIView> _topLeftCornerLines;
-        List<UIView> _topRightCornerLines;
-        List<UIView> _bottomLeftCornerLines;
-        List<UIView> _bottomRightCornerLines;
+        private List<UIView> _topLeftCornerLines;
+        private List<UIView> _topRightCornerLines;
+        private List<UIView> _bottomLeftCornerLines;
+        private List<UIView> _bottomRightCornerLines;
 
-        List<UIButton> _cornerButtons;
+        private List<UIButton> _cornerButtons;
 
-        float _cornerLineDepth = 3.0f;
-        float _cornerLineWidth = 22.5f;
-        float _lineWidth = 1;
-        float _outterGapRatio = 1 / 3;
-        public float CornerButtonWidth => 60;
-        public float OutterGap => CornerButtonWidth * _outterGapRatio;
+        private const float CornerLineDepth = 3.0f;
+        private const float CornerLineWidth = 22.5f;
+        private const float LineWidth = 1;
+        private const float OuterGapRatio = 1 / 3;
+        private const float CornerButtonSize = 60;
+        private float OuterGap => CornerButtonSize * OuterGapRatio;
+        private CGSize _minimumSize = CGSize.Empty;
+        
         public bool IsResizable = false;
         public bool IsMovable = false;
-        public CGSize MinimumSize = CGSize.Empty;
-        public bool SelectionLock = false;
 
         public CropOverlayView(CGRect frame) : base(frame)
         {
@@ -42,12 +42,12 @@ namespace Nyris.UI.iOS.Crop
         {
         }
 
-        void Setup()
+        private void Setup()
         {
             CreateLines();
         }
-        
-        void CreateLines()
+
+        private void CreateLines()
         {
             _outerLines = new List<UIView>
             {
@@ -123,30 +123,30 @@ namespace Nyris.UI.iOS.Crop
             return button;
         }
 
-        void GenerateLineFrame()
+        private void GenerateLineFrame()
         {
-            for (int i = 0; i < _outerLines.Count; i++)
+            for (var i = 0; i < _outerLines.Count; i++)
             {
                 var line = _outerLines[i];
                 CGRect lineFrame;
                 switch (i)
                 {
                     case 0:
-                        lineFrame = new CGRect(OutterGap, OutterGap, Bounds.Width - OutterGap * 2, _lineWidth);
+                        lineFrame = new CGRect(OuterGap, OuterGap, Bounds.Width - OuterGap * 2, LineWidth);
                         break;
 
                     case 1:
-                        lineFrame = new CGRect(Bounds.Width - _lineWidth - OutterGap, OutterGap, _lineWidth,
+                        lineFrame = new CGRect(Bounds.Width - LineWidth - OuterGap, OuterGap, LineWidth,
                             Bounds.Height);
                         break;
 
                     case 2:
-                        lineFrame = new CGRect(OutterGap, Bounds.Height - _lineWidth - OutterGap,
-                            Bounds.Width - OutterGap * 2, _lineWidth);
+                        lineFrame = new CGRect(OuterGap, Bounds.Height - LineWidth - OuterGap,
+                            Bounds.Width - OuterGap * 2, LineWidth);
                         break;
 
                     case 3:
-                        lineFrame = new CGRect(OutterGap, OutterGap, _lineWidth, Bounds.Height - OutterGap * 2);
+                        lineFrame = new CGRect(OuterGap, OuterGap, LineWidth, Bounds.Height - OuterGap * 2);
                         break;
                     default:
                         lineFrame = CGRect.Empty;
@@ -157,7 +157,7 @@ namespace Nyris.UI.iOS.Crop
             }
         }
 
-        void GenerateCorners()
+        private void GenerateCorners()
         {
             var corners = new List<List<UIView>>
             {
@@ -167,58 +167,56 @@ namespace Nyris.UI.iOS.Crop
                 _bottomRightCornerLines,
             };
 
-            for (int i = 0; i < corners.Count; i++)
+            for (var i = 0; i < corners.Count; i++)
             {
                 var corner = corners[i];
 
                 var horizontalFrame = CGRect.Empty;
                 var verticalFrame = CGRect.Empty;
                 var buttonFrame = CGRect.Empty;
-                var buttonSize = new CGSize(CornerButtonWidth, CornerButtonWidth);
-                var buttonPadding = 10f;
+                var buttonSize = new CGSize(CornerButtonSize, CornerButtonSize);
+                const float buttonPadding = 10f;
 
                 switch (i)
                 {
                     case 0:
-                        verticalFrame = new CGRect(OutterGap, OutterGap, _cornerLineDepth, _cornerLineWidth);
-                        horizontalFrame = new CGRect(OutterGap, OutterGap, _cornerLineWidth, _cornerLineDepth);
+                        verticalFrame = new CGRect(OuterGap, OuterGap, CornerLineDepth, CornerLineWidth);
+                        horizontalFrame = new CGRect(OuterGap, OuterGap, CornerLineWidth, CornerLineDepth);
                         buttonFrame = new CGRect(new CGPoint(- buttonPadding, - buttonPadding), buttonSize);
                         break;
 
                     case 1:
-                        verticalFrame = new CGRect(Bounds.Width - _cornerLineDepth - OutterGap, OutterGap,
-                            _cornerLineDepth, _cornerLineWidth);
-                        horizontalFrame = new CGRect(Bounds.Width - _cornerLineWidth - OutterGap, OutterGap,
-                            _cornerLineWidth, _cornerLineDepth);
-                        buttonFrame = new CGRect(new CGPoint(Bounds.Width - CornerButtonWidth + buttonPadding, -buttonPadding), buttonSize);
+                        verticalFrame = new CGRect(Bounds.Width - CornerLineDepth - OuterGap, OuterGap,
+                            CornerLineDepth, CornerLineWidth);
+                        horizontalFrame = new CGRect(Bounds.Width - CornerLineWidth - OuterGap, OuterGap,
+                            CornerLineWidth, CornerLineDepth);
+                        buttonFrame = new CGRect(new CGPoint(Bounds.Width - CornerButtonSize + buttonPadding, -buttonPadding), buttonSize);
                         break;
 
                     case 2:
-                        verticalFrame = new CGRect(OutterGap, Bounds.Height - _cornerLineWidth - OutterGap,
-                            _cornerLineDepth, _cornerLineWidth);
-                        horizontalFrame = new CGRect(OutterGap, Bounds.Height - _cornerLineDepth - OutterGap,
-                            _cornerLineWidth, _cornerLineDepth);
-                        buttonFrame = new CGRect(new CGPoint(-buttonPadding, Bounds.Height - CornerButtonWidth + buttonPadding), buttonSize);
+                        verticalFrame = new CGRect(OuterGap, Bounds.Height - CornerLineWidth - OuterGap,
+                            CornerLineDepth, CornerLineWidth);
+                        horizontalFrame = new CGRect(OuterGap, Bounds.Height - CornerLineDepth - OuterGap,
+                            CornerLineWidth, CornerLineDepth);
+                        buttonFrame = new CGRect(new CGPoint(-buttonPadding, Bounds.Height - CornerButtonSize + buttonPadding), buttonSize);
                         break;
 
                     case 3:
                         verticalFrame = new CGRect(
-                            Bounds.Width - _cornerLineDepth - OutterGap,
-                            Bounds.Height - _cornerLineWidth - OutterGap,
-                            _cornerLineDepth,
-                            _cornerLineWidth
+                            Bounds.Width - CornerLineDepth - OuterGap,
+                            Bounds.Height - CornerLineWidth - OuterGap,
+                            CornerLineDepth,
+                            CornerLineWidth
                         );
                         horizontalFrame = new CGRect(
-                            Bounds.Width - _cornerLineWidth - OutterGap,
-                            Bounds.Height - _cornerLineDepth - OutterGap,
-                            _cornerLineWidth,
-                            _cornerLineDepth
+                            Bounds.Width - CornerLineWidth - OuterGap,
+                            Bounds.Height - CornerLineDepth - OuterGap,
+                            CornerLineWidth,
+                            CornerLineDepth
                         );
                         buttonFrame = new CGRect(
-                            new CGPoint(Bounds.Width - CornerButtonWidth + buttonPadding, Bounds.Height - CornerButtonWidth + buttonPadding),
+                            new CGPoint(Bounds.Width - CornerButtonSize + buttonPadding, Bounds.Height - CornerButtonSize + buttonPadding),
                             buttonSize);
-                        break;
-                    default:
                         break;
                 }
 
@@ -229,7 +227,7 @@ namespace Nyris.UI.iOS.Crop
             }
         }
 
-        void MoveCropOverlay(UIPanGestureRecognizer gestureRecognizer)
+        private void MoveCropOverlay(UIPanGestureRecognizer gestureRecognizer)
         {
             if (IsResizable && (gestureRecognizer.View is UIButton))
             {
@@ -273,8 +271,8 @@ namespace Nyris.UI.iOS.Crop
                     }
 
                     var minimumFrame = new CGRect(newFrame.X, newFrame.Y,
-                        Math.Max(newFrame.Width, MinimumSize.Width + 2 * OutterGap),
-                        Math.Max(newFrame.Height, MinimumSize.Height + 2 * OutterGap));
+                        Math.Max(newFrame.Width, _minimumSize.Width + 2 * OuterGap),
+                        Math.Max(newFrame.Height, _minimumSize.Height + 2 * OuterGap));
                     Frame = minimumFrame;
                     LayoutSubviews();
                     gestureRecognizer.SetTranslation(CGPoint.Empty, this);
@@ -296,23 +294,23 @@ namespace Nyris.UI.iOS.Crop
                 var xMovement = gestureRecognizer.View.Center.X + translation.X;
                 var yMovement = gestureRecognizer.View.Center.Y + translation.Y;
 
-                xMovement = CanMoveHorizontaly(xMovement) ? xMovement : gestureRecognizer.View.Center.X - translation.X;
-                yMovement = CanMoveVerticaly(yMovement) ? yMovement : gestureRecognizer.View.Center.Y - translation.Y;
+                xMovement = CanMoveHorizontally(xMovement) ? xMovement : gestureRecognizer.View.Center.X - translation.X;
+                yMovement = CanMoveVertically(yMovement) ? yMovement : gestureRecognizer.View.Center.Y - translation.Y;
 
                 gestureRecognizer.View.Center = new CGPoint(xMovement, yMovement);
                 gestureRecognizer.SetTranslation(CGPoint.Empty, this);
             }
         }
 
-        bool CanMoveHorizontaly(nfloat position)
+        private bool CanMoveHorizontally(nfloat position)
         {
-            var width = Frame.Width - (2 * OutterGap);
+            var width = Frame.Width - (2 * OuterGap);
             return position - (width * 0.5) >= 0 && position + (width * 0.5) <= _screenWidth;
         }
 
-        bool CanMoveVerticaly(nfloat position)
+        private bool CanMoveVertically(nfloat position)
         {
-            var height = Frame.Height - (2 * OutterGap);
+            var height = Frame.Height - (2 * OuterGap);
             return position - (height * 0.5) >= 0 && position + (height * 0.5) <= (_screenHeight);
         }
 
@@ -321,11 +319,11 @@ namespace Nyris.UI.iOS.Crop
             base.LayoutSubviews();
             GenerateLineFrame();
             GenerateCorners();
-            var lineThickness = _lineWidth / UIScreen.MainScreen.Scale;
+            var lineThickness = LineWidth / UIScreen.MainScreen.Scale;
             var vPadding = Bounds.Height / (_verticalLines.Count + 1);
             var hPadding = Bounds.Width / (_horizontalLines.Count + 1);
 
-            for (int i = 0; i < _horizontalLines.Count; i++)
+            for (var i = 0; i < _horizontalLines.Count; i++)
             {
                 var hLine = _horizontalLines[i];
                 var vLine = _verticalLines[i];
@@ -333,26 +331,14 @@ namespace Nyris.UI.iOS.Crop
                 var vSpacing = (vPadding * (i + 1)) + (lineThickness * i);
                 var hSpacing = (hPadding * (i + 1)) + (lineThickness * i);
 
-                hLine.Frame = new CGRect(OutterGap, vSpacing + OutterGap, Bounds.Width - OutterGap * 2, lineThickness);
-                vLine.Frame = new CGRect(hSpacing + OutterGap, OutterGap, lineThickness, Bounds.Height - OutterGap * 2);
+                hLine.Frame = new CGRect(OuterGap, vSpacing + OuterGap, Bounds.Width - OuterGap * 2, lineThickness);
+                vLine.Frame = new CGRect(hSpacing + OuterGap, OuterGap, lineThickness, Bounds.Height - OuterGap * 2);
             }
         }
 
         public override UIView HitTest(CGPoint point, UIEvent uievent)
         {
             var currentView = base.HitTest(point, uievent);
-            if (currentView == null)
-            {
-            }
-            else
-            {
-            }
-
-
-            if (!IsMovable && IsResizable && currentView != null)
-            {
-            }
-
             return currentView;
         }
     }
